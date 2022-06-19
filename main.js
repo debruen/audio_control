b452e46879bc2ccbecfda3b9e2164732246aa830
debruen <florian@debruen.com>
2022-06-19 22:58:58 +0200

    0.0.0

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path')
+
+const Audio = require('audio')
+const audio = new Audio
 
 function createWindow () {
   // Create the browser window.
@@ -39,5 +42,17 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
+
+ipcMain.on('input', async (err, data) => {
+  if (err) console.log(err)
+  const result = await audio.input(data)
+  mainWindow.webContents.send('input', result)
+})
+
+ipcMain.on('output', async (err, data) => {
+  if (err) console.log(err)
+  const result = await audio.output(data)
+  mainWindow.webContents.send('output', result)
+})
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
